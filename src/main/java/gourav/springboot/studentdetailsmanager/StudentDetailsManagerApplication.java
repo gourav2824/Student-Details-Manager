9b3e3d335a10dba230b2669af582955405bce52d
Gourav <gourav2824@gmail.com>
2023-02-02 18:08:28 +0530

    Add: initial commit

---
+++ src/main/java/gourav/springboot/studentdetailsmanager/StudentDetailsManagerApplication.java
@@ -0,0 +1,11 @@
+package gourav.springboot.studentdetailsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDetailsManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDetailsManagerApplication.class, args);
+	}
+}
