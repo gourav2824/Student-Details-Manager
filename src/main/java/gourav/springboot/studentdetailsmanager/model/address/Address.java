cf02d8411e46bd0ee25aca40d236f22891c19120
Gourav <gourav2824@gmail.com>
2023-02-03 00:45:48 +0530

    Add: add model classes for Student and Address

---
+++ src/main/java/gourav/springboot/studentdetailsmanager/model/address/Address.java
@@ -0,0 +1,25 @@
+package gourav.springboot.studentdetailsmanager.model.address;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String street;
+
+    private String city;
+
+    protected Address() {
+    }
+}
---
+++ src/main/java/gourav/springboot/studentdetailsmanager/model/student/Student.java
@@ -0,0 +1,29 @@
+package gourav.springboot.studentdetailsmanager.model.student;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private int addressId;
+
+    protected Student() {
+    }
+}
