d9f7c93949d758cce1e9f8ba5bd585e57861b188
Gourav <gourav2824@gmail.com>
2023-02-04 02:10:17 +0530

    Test: add test to get address

--- src/test/java/gourav/springboot/studentdetailsmanager/service/AddressServiceTest.java
+++ src/test/java/gourav/springboot/studentdetailsmanager/service/AddressServiceTest.java
@@ -3,10 +3,13 @@ package gourav.springboot.studentdetailsmanager.service;
 import gourav.springboot.studentdetailsmanager.model.Address;
 import gourav.springboot.studentdetailsmanager.repository.AddressRepository;
 import gourav.springboot.studentdetailsmanager.request.CreateAddressRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -18,12 +21,18 @@ class AddressServiceTest {
     @Mock
     private AddressRepository addressRepository;
 
+    private AddressService addressService;
+
+    @BeforeEach
+    void setUp() {
+        addressService = new AddressService(addressRepository);
+    }
+
     @Test
     void shouldBeAbleToCreateAddress() {
         final Address address = new Address("New Street", "New Delhi");
         when(addressRepository.save(any(Address.class))).thenReturn(address);
 
-        final AddressService addressService = new AddressService(addressRepository);
         final CreateAddressRequest createAddressRequest = new CreateAddressRequest(address.getStreet(), address.getCity());
 
         final Address actualAddress = addressService.createAddress(createAddressRequest);
@@ -31,5 +40,17 @@ class AddressServiceTest {
         assertThat(actualAddress.getId()).isNull();
         assertThat(actualAddress.getStreet()).isEqualTo(createAddressRequest.getStreet());
         assertThat(actualAddress.getCity()).isEqualTo(createAddressRequest.getCity());
+    }
+
+    @Test
+    void shouldBeAbleToGetAddress() {
+        final int addressId = 1;
+        final Address address = new Address("New Street", "New Delhi");
+        when(addressRepository.findById(addressId)).thenReturn(Optional.of(address));
+
+        final Address actualAddress = addressService.getAddress(addressId);
+
+        assertThat(actualAddress.getStreet()).isEqualTo(address.getStreet());
+        assertThat(actualAddress.getCity()).isEqualTo(address.getCity());
     }
 }
