1a5ff6b6a4d1d33b37c4be4c1de180300a1ec00f
Gourav <gourav2824@gmail.com>
2023-02-05 23:20:11 +0530

    Test create student of student service

--- src/test/java/gourav/springboot/studentdetailsmanager/service/StudentServiceTest.java
+++ src/test/java/gourav/springboot/studentdetailsmanager/service/StudentServiceTest.java
@@ -3,7 +3,10 @@ package gourav.springboot.studentdetailsmanager.service;
 import gourav.springboot.studentdetailsmanager.model.Address;
 import gourav.springboot.studentdetailsmanager.model.Student;
 import gourav.springboot.studentdetailsmanager.repository.StudentRepository;
+import gourav.springboot.studentdetailsmanager.request.CreateAddressRequest;
+import gourav.springboot.studentdetailsmanager.request.CreateStudentRequest;
 import gourav.springboot.studentdetailsmanager.response.StudentResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -12,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +30,13 @@ class StudentServiceTest {
     @Mock
     private AddressService addressService;
 
+    private StudentService studentService;
+
+    @BeforeEach
+    void setUp() {
+        studentService = new StudentService(studentRepository, addressService);
+    }
+
     @Test
     void shouldBeAbleToGetStudent() {
         final int studentId = 1, addressId = 10;
@@ -34,8 +45,19 @@ class StudentServiceTest {
         when(studentRepository.findById(studentId)).thenReturn(Optional.of(student));
         when(addressService.getAddress(addressId)).thenReturn(address);
 
-        final StudentService studentService = new StudentService(studentRepository, addressService);
         final StudentResponse studentResponse = studentService.getStudent(studentId);
+
+        assertThat(studentResponse.getId()).isEqualTo(studentId);
+    }
+
+    @Test
+    void shouldBeAbleToCreateStudent() {
+        final int studentId = 1;
+        when(student.getId()).thenReturn(studentId);
+        when(addressService.createAddress(any(CreateAddressRequest.class))).thenReturn(address);
+        when(studentRepository.save(any(Student.class))).thenReturn(student);
+
+        final StudentResponse studentResponse = studentService.createStudent(new CreateStudentRequest());
 
         assertThat(studentResponse.getId()).isEqualTo(studentId);
     }
